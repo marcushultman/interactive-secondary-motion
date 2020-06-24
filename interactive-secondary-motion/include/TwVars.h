fea48fd1d8a4f6b83684f1cf44cab013cec042d7
Marcus Hultman <marcus.hultman1@gmail.com>
2020-06-24 22:48:13 +0200

    clang-format TwVars.h

--- interactive-secondary-motion/include/TwVars.h
+++ interactive-secondary-motion/include/TwVars.h
@@ -1,18 +1,17 @@
-/*
-* Copyright (c) 2015, Marcus Hultman
-*/
+// Copyright (c) 2015, Marcus Hultman
+
 #pragma once
 
-struct TwVars{
-	double	info_fps;
-	float	anim_speed = 0.6f;
-	float	sim_gravity = 0;
-	bool	sim_enable = true;
-	bool	sim_step = false;
-	bool	blend_useWeights = true;
-	float	blend_multiplier = .8f;
-	float	constraint_stiffness = 1.0f;
-	bool	render_wireframe = true;
-	bool	render_fill = true;
-	bool	render_distMag = false;
+struct TwVars {
+  double info_fps;
+  float anim_speed = 0.6f;
+  float sim_gravity = 0;
+  bool sim_enable = true;
+  bool sim_step = false;
+  bool blend_useWeights = true;
+  float blend_multiplier = .8f;
+  float constraint_stiffness = 1.0f;
+  bool render_wireframe = true;
+  bool render_fill = true;
+  bool render_distMag = false;
 };
